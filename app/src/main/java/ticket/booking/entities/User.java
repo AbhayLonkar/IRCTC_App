2319b58fe824aff696769993000f656b212eef00
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-07-26 13:10:04 +0530

    adds getter and setter for entities

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -8,4 +8,57 @@ public class User {
     private String hashPassword;
     private List<Ticket> ticketsBooked;
     private String userId;
+
+    public User(String name, String password, String hashPassword, List<Ticket> ticketsBooked, String userId) {
+        this.name = name;
+        this.password = password;
+        this.hashPassword = hashPassword;
+        this.ticketsBooked = ticketsBooked;
+        this.userId = userId;
+    }
+
+    public User() {}
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getHashPassword() {
+        return this.hashPassword;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return this.ticketsBooked;
+    }
+
+    public void printTickets() {
+        for(int i=0; i<this.ticketsBooked.size(); i++) {
+            System.out.println(ticketsBooked.get(i).getTicketInfo());
+        }
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+
+    public void setTicketsBooked(List<Ticket> ticketsBooked) {
+        this.ticketsBooked = ticketsBooked;
+    }
+
+    public void setUserId() {
+        this.userId = userId;
+    }
+
 }
